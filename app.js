82c3384e68c3aa387c4ec4b56d3af1309aed1f62
Prachi Polakhare <prachi24599@gmail.com>
2022-12-26 13:12:38 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const https = require("https");
+
+const app = express();
+
+app.get("/", function(req, res){
+    const url = "https://api.openweathermap.org/data/2.5/weather?appid=03fe1538c61eb075c2b0eaa4404056ad&q=landon&units=metric";
+    https.get(url, function(response){
+        console.log(response.statusCode);
+        //console.log(response.headers);
+
+        response.on("data", function(data){
+            //console.log(data); - gives data in hexadecimal
+            const weatherData = JSON.parse(data); //converts hex into string json
+            const temp = weatherData.main.temp;
+            const desc = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const iconURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+            res.write("<p>The weather is currently " + desc + "</p>");
+            res.write("<h1>Tempture in Landon is " + temp + " degrees celcius</h1>");
+            res.write("<img src = " + iconURL +" >");
+            res.send();
+        });
+    });
+});
+
+app.listen("3000", function(){
+    console.log("Server is running on port 3000");
+});
